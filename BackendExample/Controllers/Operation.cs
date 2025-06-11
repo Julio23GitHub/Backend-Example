0143217db3293bbd46e0520bb99f18a749ff76d7
Julio Nuñez <98823033+JulioDejesus@users.noreply.github.com>
2025-06-11 13:50:47 -0300

    Clase de Controladores

---
+++ BackendExample/Controllers/Operation.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackendExample.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class Operation : ControllerBase
+	{
+		// GET: api/<Operation>
+		[HttpGet]
+		public decimal Get(decimal a, decimal b)
+		{
+			return a + b;
+		}
+
+
+		// POST api/<Operation>
+		[HttpPost]
+		public decimal Post(Number number, [FromHeader] string Host, [FromHeader(Name = "Content-Length")] string ContentLength
+			, [FromHeader(Name = "x-some")] string Some)
+		{
+			Console.WriteLine($"The host is {Host} {ContentLength}  {Some}");
+			return number.a - number.b;
+		}
+
+		[HttpPut]
+		public decimal Edit(Number number)
+		{
+			return number.a * number.b;
+		}
+
+		[HttpDelete]
+		public decimal Delete(decimal a, decimal b)
+		{
+			return a / b;
+		}
+
+	}
+
+	public class Number
+	{
+		public decimal a { get; set; }
+		public decimal b { get; set; }
+	}
+}
